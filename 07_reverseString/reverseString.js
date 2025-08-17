db821002f67a46cbeeffef84fd9f5d0dd9d0a588
Cyril Josef Tinae <cyriljoseft@gmail.com>
2025-08-17 23:52:51 +0800

    Complete 07 reverseString exercise.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = string.split('');
+    let reversedArray = [];
 
+    for (let i = array.length - 1; i >=0 ; i--) {
+        reversedArray.push(array[i]);
+    }
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
