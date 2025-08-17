7e145a316d547708eeee440c076fd9d9a9700b31
Cyril Josef Tinae <cyriljoseft@gmail.com>
2025-08-18 00:24:50 +0800

    Complete 09 sumAll exercise.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (typeof int1 !== 'number' || typeof int2 !== 'number')
+        return 'ERROR';
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+    if (int1 % 1 !== 0 || int2 % 1 !== 0) {
+        return 'ERROR';
+    }
 
+    let start = int1
+    let end = int2;
+    int1 > int2 ? (start = int2, end = int1) : (start = int1, end = int2);
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
