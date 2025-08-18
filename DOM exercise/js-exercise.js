87c2fa9848362e951084822a5ee891372cdbe6c7
Cyril Josef Tinae <cyriljoseft@gmail.com>
2025-08-18 10:10:05 +0800

    Create .html and .js exercise files.

---
+++ DOM exercise/js-exercise.js
@@ -0,0 +1,33 @@
+const container = document.getElementById('container');
+
+const redText = document.createElement('p');
+redText.textContent = "Hey! I'm red!";
+container.appendChild(redText);
+
+const blueText = document.createElement('h3');
+blueText.textContent = "I'm a blue h3!";
+container.appendChild(blueText);
+
+// main div
+const container2 = document.createElement('div');
+
+// children
+const heading1 = document.createElement('h1');
+heading1.textContent = "I'm in a div!";
+
+const para = document.createElement('p');
+para.textContent = "ME TOO!";
+
+// add children to div
+container2.append(heading1);
+container2.append(para);
+
+// add container2 to main container
+container.appendChild(container2);
+
+
+
+
+
+
+
